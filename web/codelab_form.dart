3b2598c32e3afbc5e48d2cbec75141cd55e02833
Shailen Tuli <shailentuli@google.com>
2014-06-17 07:05:33 -0700

    Added comments

--- web/codelab_form.dart
+++ web/codelab_form.dart
@@ -2,22 +2,38 @@ import 'package:polymer/polymer.dart';
 import 'model.dart' show Codelab;
 import 'dart:html' show CustomEvent, Event, Node;
 
+/*
+ * The class for creating or updating a codelab. Performs validation based on
+ * a codelab based on validation rules defined in the model.
+ */
 @CustomTag('codelab-form')
 class CodelabFormElement extends PolymerElement {
-  // TODO: change codelab constructor to make title optional.
+  /*
+   * The Codelab object modified by this form.
+   */
   @published Codelab codelab;
-  @observable List<String> get allLevels => toObservable(Codelab.LEVELS);
 
+  /*
+   * Getters that make Codelab static values accessible in the template.
+   */
+  @observable List<String> get allLevels => toObservable(Codelab.LEVELS);
   int get minTitleLength =>  Codelab.MIN_TITLE_LENGTH;
   int get maxTitleLength => Codelab.MAX_TITLE_LENGTH;
   int get maxDescriptionLength => Codelab.MAX_DESCRIPTION_LENGTH;
 
+  /*
+   * Variables used in displaying error messages.
+   */
   @observable String formErrorMessage = '';
   @observable String titleErrorMessage = '';
   @observable String descriptionErrorMessage = '';
 
   CodelabFormElement.created() : super.created() {}
 
+  /*
+   * Validates the codelab title. If title is not valid, sets error message and
+   * returns false. Otherwise, removes error message and returns true.
+   */
   bool validateTitle() {
     if (codelab.title.length < minTitleLength ||
         codelab.title.length > maxTitleLength) {
@@ -29,6 +45,11 @@ class CodelabFormElement extends PolymerElement {
     return true;
   }
 
+  /*
+   * Validates the codelab description. If description is not valid, sets error
+   * message and returns false. Otherwise, removes error message and returns
+   * true.
+   */
   bool validateDescription() {
     if (codelab.description.length > maxDescriptionLength) {
       descriptionErrorMessage = "Description cannot be more than "
@@ -39,20 +60,26 @@ class CodelabFormElement extends PolymerElement {
     return true;
   }
 
-  // createOrUpdate() ?
-  create(Event event, Object detail, Node sender) {
+  /*
+   * Dispatches a custom event if a codelab passes validation. Otherwise, sets
+   * the form error message.
+   */
+  validateCodelab(Event event, Object detail, Node sender) {
     event.preventDefault();
     if (validateTitle() && validateDescription()) {
       formErrorMessage = '';
       // TODO: better name.
-      dispatchEvent(new CustomEvent('codelabcreated',
+      dispatchEvent(new CustomEvent('codelabvalidated',
           detail: {'codelab': codelab}));
     } else {
       formErrorMessage = 'Error: codelab not saved.';
     }
   }
 
-  cancel(Event event, Object detail, Node sender) {
+  /*
+   * Dispatches a custom event when a form is no longer needed.
+   */
+  cancelForm(Event event, Object detail, Node sender) {
     event.preventDefault();
     dispatchEvent(new CustomEvent('formnotneeded'));
   }
--- web/codelab_list.dart
+++ web/codelab_list.dart
@@ -4,26 +4,29 @@ import 'dart:html';
 
 @CustomTag('codelab-list')
 class CodelabList extends PolymerElement {
-
-  @observable Codelab newCodelab = new Codelab('');
+  static const ALL = "all";
+  @observable Codelab newCodelab = new Codelab();
   @observable List<Codelab> codelabs = toObservable([]);
   @observable get filters {
-    List<String> f = ['all'];
-    f.addAll(Codelab.LEVELS);
-    return f;
+    List<String> _filters = [ALL];
+    _filters.addAll(Codelab.LEVELS);
+    return _filters;
   }
-  @observable String filterValue = 'all';
+  @observable String filterValue = ALL;
   @observable List<Codelab> filteredCodelabs = toObservable([]);
 
   CodelabList.created() : super.created() {
-    // Set a default level.
+    // Set a default level. Assume most codelabs are 'intermediate'.
     newCodelab.level = Codelab.LEVELS[1];
     filteredCodelabs = codelabs;
   }
 
+  /*
+   * Calculates the codelabs to display when a filter is used.
+   */
   filter() {
-    if (filterValue == 'all') {
-      filteredCodelabs = codelabs;  // By value????????
+    if (filterValue == ALL) {
+      filteredCodelabs = codelabs;
       return;
     }
     filteredCodelabs = codelabs.where((codelab) {
@@ -31,19 +34,32 @@ class CodelabList extends PolymerElement {
     }).toList();
   }
 
+  /*
+   * Adds a codelab to `codelabs` and resest the new codelab form.
+   */
   addCodelab(Event e, var detail, Node sender) {
     codelabs.add(detail['codelab']);
-    newCodelab = new Codelab('');
+    resetNewCodelabForm();
   }
 
+  /*
+   * Refreshes `filteredCodelabs` every time `codelabs` changes.
+   */
   codelabsChanged() {
     filter();
   }
 
-  resetNewCodelabForm(Event e, var detail, Node sender) {
-    newCodelab = new Codelab('');
+  /*
+   * Replaces the existing `newCodelab` object with a new one, resetting form
+   * values.
+   */
+  resetNewCodelabForm() {
+    newCodelab = new Codelab();
   }
 
+  /*
+   * Removes a codelab from `codelabs`.
+   */
   deleteCodelab(Event e, var detail, Node sender) {
     var codelab = detail['codelab'];
     codelabs.remove(codelab);
