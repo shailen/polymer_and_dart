5ea175dfa3395062425d51eac7839413d48e0d59
Shailen Tuli <shailentuli@google.com>
2014-06-17 07:23:51 -0700

    Tweaks

--- web/codelab_list.dart
+++ web/codelab_list.dart
@@ -2,17 +2,40 @@ import 'package:polymer/polymer.dart';
 import 'model.dart' show Codelab;
 import 'dart:html';
 
+/*
+ * Class to represent a collection of Codelab objects.
+ */
 @CustomTag('codelab-list')
 class CodelabList extends PolymerElement {
   static const ALL = "all";
+  /*
+   * Field for a new Codelab object.
+   */
   @observable Codelab newCodelab = new Codelab();
+
+  /*
+   * Collection of codelabs. The source of truth for all codelabs in this app.
+   */
   @observable List<Codelab> codelabs = toObservable([]);
+
+  /*
+   * List of filter values. Includes the levels defined in the model, as well
+   * as a filter to return all codelabs.
+   */
   @observable get filters {
     List<String> _filters = [ALL];
     _filters.addAll(Codelab.LEVELS);
     return _filters;
   }
+
+  /*
+   * String that stores the value used to filter codelabs
+   */
   @observable String filterValue = ALL;
+
+  /*
+   * The list of filtered codelabs.
+   */
   @observable List<Codelab> filteredCodelabs = toObservable([]);
 
   CodelabList.created() : super.created() {
@@ -22,7 +45,7 @@ class CodelabList extends PolymerElement {
   }
 
   /*
-   * Calculates the codelabs to display when a filter is used.
+   * Calculates the codelabs to display when using a filter.
    */
   filter() {
     if (filterValue == ALL) {
@@ -35,7 +58,8 @@ class CodelabList extends PolymerElement {
   }
 
   /*
-   * Adds a codelab to `codelabs` and resest the new codelab form.
+   * Adds a codelab to the codelabs list and resets the new codelab form. This
+   * triggers codelabsChanged().
    */
   addCodelab(Event e, var detail, Node sender) {
     codelabs.add(detail['codelab']);
@@ -43,22 +67,21 @@ class CodelabList extends PolymerElement {
   }
 
   /*
-   * Refreshes `filteredCodelabs` every time `codelabs` changes.
+   * Refreshes the filtered codelabs list every time the codelabs list changes.
    */
   codelabsChanged() {
     filter();
   }
 
   /*
-   * Replaces the existing `newCodelab` object with a new one, resetting form
-   * values.
+   * Replaces the existing new Codelab, causing the new codelab form to reset.
    */
   resetNewCodelabForm() {
     newCodelab = new Codelab();
   }
 
   /*
-   * Removes a codelab from `codelabs`.
+   * Removes a codelab from the codelabs list. This triggers codelabsChanged().
    */
   deleteCodelab(Event e, var detail, Node sender) {
     var codelab = detail['codelab'];
