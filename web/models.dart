61dc2e04ecb0386f00414d4e86595786e8ecd7a9
Shailen Tuli <shailentuli@google.com>
2014-06-16 17:38:20 -0700

    Removed topics functionality

--- web/models.dart
+++ web/models.dart
@@ -3,10 +3,6 @@ library polymer_and_dart.web.models;
 import 'package:polymer/polymer.dart';
 
 class Codelab extends Observable {
-  static const List<String> TOPICS = const [
-      'Accessibility', 'Android', 'Chrome / Web', 'Cloud Services', 'Dart',
-      'Games', 'Go', 'Growth','Location', 'Monetization', 'Media',
-      'Performance', 'Polymer', 'Tools', 'UX', 'Wearables'];
   static const List<String> THEMES = const ['Design', 'Develop', 'Distribute'];
   @observable String title;
   @observable String description;
