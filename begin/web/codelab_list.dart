3613a372b2fd9f562cfb8cc1a8ed11e1b4564b6e
Shailen Tuli <shailentuli@google.com>
2014-06-17 19:15:07 -0700

    Added begin and end directories

---
+++ begin/web/codelab_list.dart
@@ -0,0 +1,14 @@
+import 'package:polymer/polymer.dart';
+import 'model.dart' show Codelab;
+import 'dart:html' show Event, Node;
+
+/*
+ * Class to represent a collection of Codelab objects.
+ */
+@CustomTag('codelab-list')
+class CodelabList extends PolymerElement {
+  // ...
+  CodelabList.created() : super.created() {
+  }
+  // ...
+}
